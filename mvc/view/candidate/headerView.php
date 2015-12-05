fe002ec608f77d7d62d0fa9f095cf8512a5ed8cc
Ferencz Veres <ferenczv8@gmail.com>
2015-12-05 14:45:35 +0200

    forgot adding navbar code

---
+++ mvc/view/candidate/headerView.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ferencz
+ * Date: 12/5/2015
+ * Time: 2:14 PM
+ */
+?>
+
+<div class="navbar">
+    <div class="col-lg-2">
+        <a href="accountView.php">Home</a>
+    </div>
+    <div class="col-lg-2">
+        <a href="editCVView.php">Edit CV</a>
+    </div>
+    <div class="col-lg-2">
+        <a href="viewCVView.php">View CV</a>
+    </div>
+</div>
