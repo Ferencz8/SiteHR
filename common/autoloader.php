adb2dff75c8d4363c9631308c0488f85d73a02fe
Amar Singh <45370347+komantech@users.noreply.github.com>
2020-10-02 01:59:24 +0530

    Doc and Code Improved

--- common/autoloader.php
+++ common/autoloader.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Created by PhpStorm.
+ * Created by Koman Tech Solutions.
  * User: Ferencz
  * Date: 11/15/2015
  * Time: 12:20 AM
@@ -10,12 +10,12 @@ include("Constants.php");
 
 function searchFile($fileName)
 {
-    $appDirectory = new RecursiveDirectoryIterator(Constants::$AppDirectory);
+    $appDirectory = new RecursiveDirectoryIterator(Constants::$AppDirectory)  ;
     foreach (new RecursiveIteratorIterator($appDirectory) as $filename => $file) {
 
         if ($file->getFilename() === $fileName . ".php") {
             $path = $filename;
-            return $path;
+            return $path  ;
         }
     }
 }
